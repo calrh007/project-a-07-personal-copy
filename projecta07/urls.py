a20c11ea50448e5f54fce77a9c3d1ff6d89c0a45
calrh007 <calrh007@gmail.com>
2021-03-11 17:22:52 -0500

    moved account urls to workout_app and added 2 dummy tests

--- projecta07/urls.py
+++ projecta07/urls.py
@@ -16,14 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.views.generic import TemplateView
-from django.contrib.auth.views import LogoutView
-
 urlpatterns = [
     path('', include('workout_app.urls')),
     path('admin/', admin.site.urls),
-
-    path('accounts/', include('allauth.urls')),
-    path('login/', TemplateView.as_view(template_name="workout_app/login.html")),
-    path('logout/', LogoutView.as_view()),
 ]
--- workout_app/urls.py
+++ workout_app/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('accounts/', include('allauth.urls')),
+    path('login/', TemplateView.as_view(template_name="workout_app/login.html")),
+    path('logout/', LogoutView.as_view()),
 ]
