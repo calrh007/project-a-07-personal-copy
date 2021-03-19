1683efc32a640dcb65437931a4d00eb0906b4cef
Ashley Crawford <acrawford317@gmail.com>
2021-03-19 18:59:06 -0400

    workout model

--- workout_app/models.py
+++ workout_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Workout(models.Model):
+    steps = models.CharField(max_length=30)
+    miles = models.CharField(max_length=30)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "steps: " + self.steps + " | miles: " + self.miles
+
