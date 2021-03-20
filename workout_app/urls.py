0513339467df27ad942091e55822e7c8aa161a77
Ashley Crawford <acrawford317@gmail.com>
2021-03-20 16:17:20 -0400

    add workout

--- workout_app/urls.py
+++ workout_app/urls.py
@@ -5,10 +5,14 @@ from . import views
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
+from .views import AddWorkoutView
+
 urlpatterns = [
     path('', views.index, name='index'),
 
     path('accounts/', include('allauth.urls')),
     path('login/', TemplateView.as_view(template_name="workout_app/login.html")),
     path('logout/', LogoutView.as_view()),
+    path('add_workout/', AddWorkoutView.as_view(), name='add_workout'),
+
 ]
--- workout_app/views.py
+++ workout_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Workout
 
 def index(request):
     context = {}
     return render(request, 'workout_app/index.html', context)
+
+class AddWorkoutView(CreateView):
+    model = Workout
+    template_name = 'add_workout.html'
+    fields = '__all__'
