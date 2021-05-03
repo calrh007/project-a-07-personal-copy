ff448494a006d438f59266d7045f4659f940f517
calrh007 <calrh007@gmail.com>
2021-05-03 04:22:54 -0400

    leaderboard rounding

--- workout_app/templatetags/custom_tags.py
+++ workout_app/templatetags/custom_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from measurement.measures import Distance, Weight
+
 register = template.Library()
 
 @register.simple_tag
@@ -7,3 +9,11 @@ def url_replace(request, field, value):
     dict_ = request.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
+
+@register.simple_tag
+def round_if_measurement(poss_measurement):
+    if type(poss_measurement) == type(Distance()):
+        return str(round(poss_measurement.mi, 3)) + ' mi'
+    if type(poss_measurement) == type(Weight()):
+        return str(round(poss_measurement.lb, 3)) + ' lb'
+    return poss_measurement
